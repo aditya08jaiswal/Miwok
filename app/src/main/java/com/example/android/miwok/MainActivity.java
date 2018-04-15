33590a0d60c9769f69a73339fe563d3aa53890d8
Aditya Jaiswal <jaiswaladitya844@gmail.com>
2018-04-15 17:56:08 +0530

    Added intent and OnClickListeners

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         TextView numbers = (TextView) findViewById(R.id.numbers);
 
         //Set a clicklistener on that View
-        numbers.setOnClickListener(new NumbersClickListener(){
+        numbers.setOnClickListener(new View.OnClickListener(){
 
            @Override
            public void onClick(View view) {
